0008f869a349ffe833c2e84856fcf9b1a683d283
Pournima <pournimagudaghe@gmail.com>
2019-07-28 11:07:37 +0530

    second JAVA class for arithmetic operation

---
+++ Basic code java/src/variablesandDatatypes/Secondjavaclass.java
@@ -0,0 +1,18 @@
+package variablesandDatatypes;
+
+public class Secondjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		int a=10;
+	    int b=20;    //+,_,/,%,*
+	    
+	    System.out.println(a+b);
+	    System.out.println(a-b);
+	    System.out.println(a*b);
+	    System.out.println(a/b);
+	    System.out.println(a%b);
+	}
+
+}
