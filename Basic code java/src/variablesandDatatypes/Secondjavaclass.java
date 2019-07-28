df1a75bb401b4f36653613a0a5367ad7f5149a5e
Pournima <pournimagudaghe@gmail.com>
2019-07-28 18:48:55 +0530

    secondjavaclass.practice

--- Basic code java/src/variablesandDatatypes/Secondjavaclass.java
+++ Basic code java/src/variablesandDatatypes/Secondjavaclass.java
@@ -6,13 +6,22 @@ public class Secondjavaclass {
 		// TODO Auto-generated method stub
      
 		int a=10;
-	    int b=20;    //+,_,/,%,*
+	    int b=20;
+	    char chara='F';
+	    double marks=67.8;
+	    String name ="Pournima";
+	    boolean married= true;
+	    //+,_,/,%,*
 	    
 	    System.out.println(a+b);
 	    System.out.println(a-b);
 	    System.out.println(a*b);
 	    System.out.println(a/b);
 	    System.out.println(a%b);
+	    System.out.println(marks);
+	    System.out.println(chara);
+	    System.out.println(name);
+	    System.out.println(married);
 	}
 
 }
