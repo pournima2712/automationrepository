2b9025cc224eab7505ba36ee8db0cc89d5e5a774
Pournima <pournimagudaghe@gmail.com>
2019-07-28 10:07:28 +0530

    commenting my first java class

---
+++ Basic code java/src/variablesandDatatypes/Firstjavaclass.java
@@ -0,0 +1,12 @@
+package variablesandDatatypes;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+          System.out.println("first selenium class");//to print anything on your console
+          
+	}
+
+}
